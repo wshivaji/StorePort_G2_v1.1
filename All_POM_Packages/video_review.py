41c5dcd27ff45a8517ac913b87a9ce86df6ed31d
ManognaMaramreddy <manogna.maramreddy@facefirst.com>
2025-08-18 18:28:04 +0530

    Merge pull request #2 from wshivaji/shivaji
    First push

--- All_POM_Packages/video_review.py
+++ All_POM_Packages/video_review.py
@@ -12,36 +12,36 @@ class VideoReview:
 
 
         #above columns like search on store,filter by event time,filter by classification
-    # def click_on_store(self):
-    #     self.page.get_by_role("textbox", name="Search by Store Name").click()
-    #     self.page.get_by_role("textbox", name="Search by Store Name").press("CapsLock")
-    #     self.page.get_by_role("textbox", name="Search by Store Name").fill("SAFEWAY 1804")
-    #     self.page.get_by_role("textbox", name="Search by Store Name").press("Enter")
-    #     self.page.get_by_role("textbox", name="Filter By Event Time in Store").click()
-    #     self.page.locator("div").filter(has_text=re.compile(r"^Filter By Event Time in Store$")).get_by_role(
-    #         "button").click()
-    #     self.page.get_by_role("button").filter(has_text=re.compile(r"^$")).first.click()
-    #     self.page.get_by_role("button", name="31").click()
-    #     self.page.get_by_role("button", name="31").press("Enter")
-    #     self.page.locator(".MuiSelect-select").first.click()
-    #     self.page.get_by_role("option", name="Confirmed", exact=True).get_by_role("paragraph").click()
-    #     self.page.get_by_role("option", name="No filters applied").press("Enter")
-    #     self.page.get_by_role("option", name="Confirmed", exact=True).get_by_role("paragraph").click()
-    #     self.page.get_by_role("row", name="Event Time in Store Store").get_by_role("button").nth(1).click()
+    def click_on_store(self):
+        self.page.get_by_role("textbox", name="Search by Store Name").click()
+        self.page.get_by_role("textbox", name="Search by Store Name").press("CapsLock")
+        self.page.get_by_role("textbox", name="Search by Store Name").fill("SAFEWAY 1804")
+        self.page.get_by_role("textbox", name="Search by Store Name").press("Enter")
+        self.page.get_by_role("textbox", name="Filter By Event Time in Store").click()
+        self.page.locator("div").filter(has_text=re.compile(r"^Filter By Event Time in Store$")).get_by_role(
+            "button").click()
+        self.page.get_by_role("button").filter(has_text=re.compile(r"^$")).first.click()
+        self.page.get_by_role("button", name="31").click()
+        self.page.get_by_role("button", name="31").press("Enter")
+        self.page.locator(".MuiSelect-select").first.click()
+        self.page.get_by_role("option", name="Confirmed", exact=True).get_by_role("paragraph").click()
+        self.page.get_by_role("option", name="No filters applied").press("Enter")
+        self.page.get_by_role("option", name="Confirmed", exact=True).get_by_role("paragraph").click()
+        self.page.get_by_role("row", name="Event Time in Store Store").get_by_role("button").nth(1).click()
 
 
-    # def validate_filtered_store_data(self):
-    #     # Applying the filter for "Store" column with value "SAFEWAY 1804"
-    #     self.page.locator("div").filter(has_text=re.compile(r"^Store$")).nth(2).click()
-    #     self.page.get_by_role("row", name="Event Time in Store Store").get_by_role("button").nth(2).hover()
-    #     self.page.get_by_role("row", name="Event Time in Store Store").get_by_role("button").nth(2).click()
-    #     self.page.get_by_role("menuitem", name="Filters").click()
-    #     self.page.locator("div").filter(has_text=re.compile(r"^Value$")).click()
-    #     self.page.get_by_role("textbox", name="Value").press("CapsLock")
-    #     self.page.get_by_role("textbox", name="Value").fill("SAFEWAY 1804")
-    #     self.page.get_by_role("button", name="APPLY FILTER").click()
-    #     self.page.wait_for_timeout(3000)
-    #     assert  self.page.locator("div:nth-child(2) > div:nth-child(2) > .MuiStack-root > .MuiBox-root > span > .MuiTypography-root").inner_text()
+    def validate_filtered_store_data(self):
+        # Applying the filter for "Store" column with value "SAFEWAY 1804"
+        self.page.locator("div").filter(has_text=re.compile(r"^Store$")).nth(2).click()
+        self.page.get_by_role("row", name="Event Time in Store Store").get_by_role("button").nth(2).hover()
+        self.page.get_by_role("row", name="Event Time in Store Store").get_by_role("button").nth(2).click()
+        self.page.get_by_role("menuitem", name="Filters").click()
+        self.page.locator("div").filter(has_text=re.compile(r"^Value$")).click()
+        self.page.get_by_role("textbox", name="Value").press("CapsLock")
+        self.page.get_by_role("textbox", name="Value").fill("SAFEWAY 1804")
+        self.page.get_by_role("button", name="APPLY FILTER").click()
+        self.page.wait_for_timeout(3000)
+        assert  self.page.locator("div:nth-child(2) > div:nth-child(2) > .MuiStack-root > .MuiBox-root > span > .MuiTypography-root").inner_text()
 
     def Ammount(self):
         self.page.get_by_text("Amount").click()
@@ -59,6 +59,7 @@ class VideoReview:
         # Check if the list is in ascending order
         is_sorted = all(amount_values[i] <= amount_values[i + 1] for i in range(len(amount_values) - 1))
         assert is_sorted
+
 
 
 
--- All_Test_Cases/test_login.py
+++ All_Test_Cases/test_login.py
@@ -1,33 +1,213 @@
+import pytest
 from playwright.sync_api import sync_playwright
 from All_POM_Packages.login_page import LoginPage
 from conftest import *
-from utilities import *
-
+import re
 
 
 class TestLogin:
-    def test_Login_to_StorePort_G2_TC01(self):
-        try:
-            with sync_playwright() as p:
-                browser = p.chromium.launch(headless=False)
-                page = browser.new_page()
-                login_page = LoginPage(page)
-                login_page.open_login_page(store_port_login_url)
-                # login_page.open_login_page('https://g2-cr-client-web-489921633482.us-central1.run.app/login')
-                login_page.fill_username(EMAIL)
-                # login_page.fill_username('manogna.maramreddy@facefirst.com')
-                login_page.click_next()
-                login_page.fill_password(PASSWORD)
-                # login_page.fill_password('realize7Guard$Him')
-                login_page.click_sign_in()
-                login_page.click_send_code()
-                login_page.click_on_checkbox_enable_access_without_MFA_for_24_hours()
-                login_page.wait_for_code_verification()
-                login_page.click_on_sign_in_button()
-                text = login_page.get_store_port_g2_heading()
-                print(text)
-        except Exception as ex:
-            print(f"ex: {type(ex).__name__}")
+    @pytest.fixture(scope="function", autouse=True)
+    def setup_browser(self):
+        self.playwright = sync_playwright().start()
+        self.browser = self.playwright.chromium.launch(headless=False)
+        self.page = self.browser.new_page()
+        self.login_page = LoginPage(self.page)
+        self.login_page.open_login_page(store_port_login_url)
+        yield
+        self.browser.close()
+        self.playwright.stop()
+    #postive test case with valid username and password
+    # def test_Login_to_StorePort_G2_TC01(self):
+    #     self.login_page.fill_username(EMAIL)
+    #     self.login_page.click_next()
+    #     self.login_page.fill_password(PASSWORD)
+    #     self.login_page.click_sign_in()
+    #     self.login_page.click_send_code()
+    #     self.login_page.click_on_checkbox_enable_access_without_MFA_for_24_hours()
+    #     self.login_page.wait_for_code_verification()
+    #     self.login_page.click_on_sign_in_button()
+    #     text = self.login_page.get_store_port_g2_heading()
+    #     print(text)
+   # giving like empty email and click on next
+   #  def test_Login_to_StorePort_G2_TC02(self):
+   #      self.login_page.empty_email()
+   #      self.login_page.click_next()
+   #      assert self.page.locator('//span[@class="icon error-16"]//following-sibling::p').inner_text()
+    # giving like empty password and click on next
+    # def test_login_to_StorePort_G2_TC03(self):
+    #     self.login_page.fill_username(EMAIL)
+    #     self.login_page.click_next()
+    #     self.login_page.empty_password()
+    #     self.login_page.click_sign_in()
+    #     assert self.page.locator('//span[@class="icon error-16"]//following-sibling::p').inner_text()
+    # giving invalid email and valid password
+    # def test_login_to_StorePort_G2_TC04(self):
+    #     self.login_page.fill_username(invalid_email)
+    #     self.login_page.click_next()
+    #     self.login_page.fill_password(PASSWORD)
+    #     self.login_page.click_sign_in()
+    #     assert self.page.locator('//span[@class="icon error-16"]//following-sibling::p').inner_text()
+    #clicking on reset password
+    def test_login_to_StorePort_G2_TC05(self):
+        self.page.get_by_role("link", name="Forgot password?").click()
+        self.page.locator("#account-recovery-username").fill("s60191821@gmail.com")
+        self.page.get_by_role("link", name="Reset via Email").click()
+        self.page.wait_for_timeout(5000)
+    #checking in email
+    def test_checking_email(self):
+        url="https://mail.google.com/mail/u/0/#inbox"
+        self.page.goto(url)
+        self.page.get_by_role("textbox", name="Email or phone").click()
+        self.page.get_by_role("textbox", name="Email or phone").fill("s60191821@gmail.com")
+        self.page.get_by_role("button", name="Next").click()
+        self.page.get_by_role("textbox", name="Enter your password").click()
+        self.page.get_by_role("textbox", name="Enter your password").fill("Maramreddy@12")
+        self.page.get_by_role("button", name="Next").click()
+        self.page.wait_for_timeout(5000)
+        self.page.get_by_role("link", name="Account password reset  -").click()
+        self.page.wait_for_timeout(5000)
+        with self.page.expect_popup() as page1_info:
+            self.page.get_by_role("link", name="Reset Password").click()
+        self.page1 = page1_info.value
+        self.page.wait_for_timeout(5000)
+        self.page1.get_by_role("textbox", name="New password").click()
+        self.page1.get_by_role("textbox", name="New password").fill("Akkiramreddy@12")
+        self.page1.get_by_role("textbox", name="Repeat password").click()
+        self.page1.get_by_role("textbox", name="Repeat password").fill("Akkiramreddy@12")
+        self.page.wait_for_timeout(5000)
+        self.page1.get_by_role("button", name="Reset Password").click()
+        self.page1.get_by_role("button", name="Send me the code").click()
+        self.page1.get_by_role("textbox", name="Verification code").click()
+        self.page.wait_for_timeout(20000)
+        self.page1.get_by_role("button", name="Verify").click()
+        self.page.wait_for_timeout(20000)
+    def test_opening_with_resetting_password(self):
+        self.login_page.fill_username(S_Email)
+        self.login_page.click_next()
+        self.page.wait_for_timeout(5000)
+        self.login_page.fill_password('Akkiramreddy@12')
+        self.login_page.click_sign_in()
+        self.page.get_by_role("button", name="Send me the code").click()
+        self.page.get_by_role("textbox", name="Verification code").click()
+        self.page.wait_for_timeout(20000)
+        self.page.get_by_role("button", name="Sign In").click()
+        self.page.wait_for_timeout(20000)
+        self.page.locator("span").filter(has_text=re.compile(r"^s$")).click()
+        assert self.page.get_by_role("menuitem", name="s s60191821@gmail.com").is_visible()
 
 
-# open_url()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
